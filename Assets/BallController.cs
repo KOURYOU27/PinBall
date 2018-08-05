1339327dc7fbe52dd2bec05aafde5dd346716a47
RyokoAraki <a-ry.27@poem.ocn.ne.jp>
2018-08-06 05:05:05 +0900

    PinBallプロジェクト　得点表示の課題
    BallController.csに得点をつけるスクリプトを記述しましたが、失敗しています。

---
+++ Assets/BallController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosZ = -6.5f;
+
+    //ゲームオーバを表示するテキスト
+    private GameObject gameoverText;
+
+    //スコア文を表示するテキスト
+    private GameObject scoreText;
+    //スコアの変数
+    int score = 0;
+    int point = 0;
+
+    // Use this for initialization
+    void Start () {
+
+        //シーン中のGameOverTextオブジェクトを取得
+        this.gameoverText = GameObject.Find("GameOverText");
+
+        //スコアの初期化
+        score = 0;
+        //シーン中のScoreTextオブジェクトを取得
+        this.scoreText = GameObject.Find("ScoreText");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //ボールが画面外に出た場合
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            //GameoverTextにゲームオーバを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+
+        }
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        // タグによって得点を変える
+        if (collision.gameObject.tag == "SmallStarTag")
+        {
+            point += 1;
+        }
+        else if (collision.gameObject.tag == "LargeStarTag")
+        {
+            point += 2;
+        }
+        else if (collision.gameObject.tag == "SmallCloudTag")
+        {
+            point += 3;
+        }
+        else if (collision.gameObject.tag == "LargeCloudTag")
+        {
+            point += 4;
+        }
+        else
+        {
+            point += 0;
+        }
+
+        score = score + point;
+
+        //ScoreTextに表示
+        this.scoreText.GetComponent<Text>().text = "SCORE = " + score;
+
+    }
+
+
+}
